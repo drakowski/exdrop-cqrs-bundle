be11529a9d0cd526c99963ca8213044f09301cb8
damian-rakowski <damian.rakowski12@o2.pl>
2025-01-10 20:10:56 +0100

    fixed

--- src/ExdropSharedBundle.php
+++ src/ExdropSharedBundle.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace DevExdrop\CommonCodeModuleBundle;
+namespace ExdropCommon;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class CommonCodeModuleBundle extends AbstractBundle
+class ExdropCommonBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
